91c3130144486c42d02a22a6f327738f3c2a1fb3
LidiaMessias <lidia.mess@gmail.com>
2024-11-15 11:53:53 -0300

    Iniciando o projeto

---
+++ projetop2/src/main/resources/schema-postgresql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS jogos(
+    id serial PRIMARY KEY,
+    nome VARCHAR(150),
+    estado VARCHAR(10),
+    preco DECIMAL(10, 2),
+    console VARCHAR(20),
+    imagem VARCHAR(200)
+);
